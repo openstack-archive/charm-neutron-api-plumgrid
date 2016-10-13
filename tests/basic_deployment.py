b7d1dbbaef6ff1b6771fe3348388b14ee25bb0f4
Bilal Baqar <bbaqar@plumgrid.com>
2016-10-14 03:08:47 +0500

    Initial commit to OpenStack gerrit
    Adding the required files to pass jobs.
    Changing configuration section in README.
    Adding .gitreview file.
    
    Change-Id: Ibac43b40aabc27fc9a740da17a57f84b9c9f4dc5
    Signed-off-by: Bilal Baqar <bbaqar@plumgrid.com>

---
+++ tests/basic_deployment.py
@@ -0,0 +1,218 @@
+# Copyright 2016 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import amulet
+import os
+import yaml
+
+from charmhelpers.contrib.openstack.amulet.deployment import (
+    OpenStackAmuletDeployment
+)
+
+from charmhelpers.contrib.openstack.amulet.utils import (
+    OpenStackAmuletUtils,
+    DEBUG,
+)
+
+# Use DEBUG to turn on debug logging
+u = OpenStackAmuletUtils(DEBUG)
+
+
+class NeutronAPIBasicDeployment(OpenStackAmuletDeployment):
+    """Amulet tests on a basic neutron-api deployment."""
+
+    def __init__(self, series, openstack=None, source=None, git=False,
+                 stable=False):
+        """Deploy the entire test environment."""
+        super(NeutronAPIBasicDeployment, self).__init__(series, openstack,
+                                                        source, stable)
+        self.git = git
+        self._add_services()
+        self._add_relations()
+        self._configure_services()
+        self._deploy()
+
+        u.log.info('Waiting on extended status checks...')
+        exclude_services = []
+        self._auto_wait_for_status(exclude_services=exclude_services)
+
+        self.d.sentry.wait()
+        self._initialize_tests()
+
+    def _assert_services(self, should_run):
+        services = ("neutron-server", "apache2", "haproxy")
+        u.get_unit_process_ids(
+            {self.neutron_api_sentry: services},
+            expect_success=should_run)
+
+    def _add_services(self):
+        """Add services
+
+           Add the services that we're testing, where neutron-api is local,
+           and the rest of the service are from lp branches that are
+           compatible with the local charm (e.g. stable or next).
+           """
+        this_service = {'name': 'neutron-api-plumgrid'}
+        other_services = [
+            {'name': 'percona-cluster', 'constraints': {'mem': '3072M'}},
+            {'name': 'rabbitmq-server'},
+            {'name': 'keystone'},
+            {'name': 'glance'},  # to satisfy workload status
+            {'name': 'nova-cloud-controller'},
+            {'name': 'neutron-api'},
+            {'name': 'nova-compute'}
+        ]
+        super(NeutronAPIBasicDeployment, self)._add_services(this_service,
+                                                             other_services)
+
+    def _add_relations(self):
+        """Add all of the relations for the services."""
+        relations = {
+            'neutron-api:shared-db': 'percona-cluster:shared-db',
+            'neutron-api:amqp': 'rabbitmq-server:amqp',
+            'neutron-api:neutron-api': 'nova-cloud-controller:neutron-api',
+            'neutron-api:identity-service': 'keystone:identity-service',
+            'neutron-api:neutron-plugin-api-subordinate': 'neutron-api-'
+                                                          'plumgrid:'
+                                                          'neutron-plugin-'
+                                                          'api-subordinate',
+            'neutron-api-plumgrid:identity-admin': 'keystone:identity-admin',
+            'keystone:shared-db': 'percona-cluster:shared-db',
+            'nova-cloud-controller:shared-db': 'percona-cluster:shared-db',
+            'nova-cloud-controller:amqp': 'rabbitmq-server:amqp',
+            'nova-compute:amqp': 'rabbitmq-server:amqp',
+            'nova-cloud-controller:identity-service': 'keystone:'
+                                                      'identity-service',
+            'nova-cloud-controller:cloud-compute': 'nova-compute:'
+                                                   'cloud-compute',
+            'glance:identity-service': 'keystone:identity-service',
+            'glance:shared-db': 'percona-cluster:shared-db',
+            'glance:amqp': 'rabbitmq-server:amqp',
+            'nova-compute:image-service': 'glance:image-service',
+            'nova-cloud-controller:image-service': 'glance:image-service',
+        }
+
+        super(NeutronAPIBasicDeployment, self)._add_relations(relations)
+
+    def _configure_services(self):
+        """Configure all of the services."""
+        neutron_api_config = {}
+        if self.git:
+            amulet_http_proxy = os.environ.get('AMULET_HTTP_PROXY')
+
+            branch = 'stable/' + self._get_openstack_release_string()
+
+            if self._get_openstack_release() >= self.trusty_kilo:
+                openstack_origin_git = {
+                    'repositories': [
+                        {'name': 'requirements',
+                         'repository': 'git://github.com/openstack/requirements',  # noqa
+                         'branch': branch},
+                        {'name': 'neutron-fwaas',
+                         'repository': 'git://github.com/openstack/neutron-fwaas',  # noqa
+                         'branch': branch},
+                        {'name': 'neutron-lbaas',
+                         'repository': 'git://github.com/openstack/neutron-lbaas',  # noqa
+                         'branch': branch},
+                        {'name': 'neutron-vpnaas',
+                         'repository': 'git://github.com/openstack/neutron-vpnaas',  # noqa
+                         'branch': branch},
+                        {'name': 'neutron',
+                         'repository': 'git://github.com/openstack/neutron',
+                         'branch': branch},
+                    ],
+                    'directory': '/mnt/openstack-git',
+                    'http_proxy': amulet_http_proxy,
+                    'https_proxy': amulet_http_proxy,
+                }
+            else:
+                reqs_repo = 'git://github.com/openstack/requirements'
+                neutron_repo = 'git://github.com/openstack/neutron'
+                if self._get_openstack_release() == self.trusty_icehouse:
+                    reqs_repo = 'git://github.com/coreycb/requirements'
+                    neutron_repo = 'git://github.com/coreycb/neutron'
+
+                openstack_origin_git = {
+                    'repositories': [
+                        {'name': 'requirements',
+                         'repository': reqs_repo,
+                         'branch': branch},
+                        {'name': 'neutron',
+                         'repository': neutron_repo,
+                         'branch': branch},
+                    ],
+                    'directory': '/mnt/openstack-git',
+                    'http_proxy': amulet_http_proxy,
+                    'https_proxy': amulet_http_proxy,
+                }
+            neutron_api_config['openstack-origin-git'] = \
+                yaml.dump(openstack_origin_git)
+
+        keystone_config = {'admin-password': 'openstack',
+                           'admin-token': 'ubuntutesting'}
+        nova_cc_config = {'network-manager': 'Neutron'}
+        pxc_config = {
+            'dataset-size': '25%',
+            'max-connections': 1000,
+            'root-password': 'ChangeMe123',
+            'sst-password': 'ChangeMe123',
+        }
+
+        configs = {
+            'neutron-api': neutron_api_config,
+            'keystone': keystone_config,
+            'percona-cluster': pxc_config,
+            'nova-cloud-controller': nova_cc_config,
+        }
+        super(NeutronAPIBasicDeployment, self)._configure_services(configs)
+
+    def _initialize_tests(self):
+        """Perform final initialization before tests get run."""
+        # Access the sentries for inspecting service units
+        self.pxc_sentry = self.d.sentry['percona-cluster'][0]
+        self.keystone_sentry = self.d.sentry['keystone'][0]
+        self.rabbitmq_sentry = self.d.sentry['rabbitmq-server'][0]
+        self.nova_cc_sentry = self.d.sentry['nova-cloud-controller'][0]
+        self.neutron_api_sentry = self.d.sentry['neutron-api'][0]
+        self.nova_compute_sentry = self.d.sentry['nova-compute'][0]
+
+        u.log.debug('openstack release val: {}'.format(
+            self._get_openstack_release()))
+        u.log.debug('openstack release str: {}'.format(
+            self._get_openstack_release_string()))
+
+    def test_100_services(self):
+        """Verify the expected services are running on the corresponding
+           service units."""
+        u.log.debug('Checking status of system services...')
+        neutron_api_services = ['neutron-server']
+
+        neutron_services = ['neutron-server']
+        nova_cc_services = ['nova-api-os-compute',
+                            'nova-cert',
+                            'nova-scheduler',
+                            'nova-conductor']
+
+        services = {
+            self.keystone_sentry: ['keystone'],
+            self.nova_cc_sentry: nova_cc_services,
+            self.neutron_api_sentry: neutron_api_services,
+        }
+
+        if self._get_openstack_release() >= self.trusty_liberty:
+            services[self.keystone_sentry] = ['apache2']
+
+        ret = u.validate_services_by_name(services)
+        if ret:
+            amulet.raise_status(amulet.FAIL, msg=ret)
