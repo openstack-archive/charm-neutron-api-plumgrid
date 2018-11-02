55d09b5a61dc419af1132da76a42287efd0086be
Bilal Baqar <mbilalbaqar@gmail.com>
2018-11-02 12:41:15 -0500

    Retire Project
    Removed everything except README.md
    
    Change-Id: Ia9e479c67ea00c971ebdde3bddb68766abb496ef
    Signed-off-by: Bilal Baqar <mbilalbaqar@gmail.com>

--- hooks/charmhelpers/contrib/openstack/exceptions.py
+++
@@ -1,6 +0,0 @@
-class OSContextError(Exception):
-    """Raised when an error occurs during context generation.
-
-    This exception is principally used in contrib.openstack.context
-    """
-    pass
